ee5a37f08577615adf5cfd04361d9963e43557b2
haker <154807387+ilyosbek-salayev@users.noreply.github.com>
2025-02-12 01:50:25 +0500

    update

--- app/(auth)/sign-in/google-signin-form.tsx
+++ app/(auth)/sign-in/google-signin-form.tsx
@@ -5,7 +5,10 @@ import { Button } from '@/components/ui/button'
 import { SignInWithGoogle } from '@/lib/actions/user.actions'
 
 export function GoogleSignInForm() {
+
+    
   const SignInButton = () => {
+
     const { pending } = useFormStatus()
     return (
       <Button disabled={pending} className='w-full' variant='outline'>
