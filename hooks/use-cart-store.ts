f4f1bc470e557372f13b2dc4242969016bd673d8
ILYOSBEK <154807387+ilyosbek-salayev@users.noreply.github.com>
2025-02-27 16:41:59 +0500

    Update use-cart-store.ts

--- hooks/use-cart-store.ts
+++ hooks/use-cart-store.ts
@@ -7,8 +7,8 @@ import { calcDeliveryDateAndPrice } from '@/lib/actions/order.actions'
 const initialState: Cart = {
   items: [],
   itemsPrice: 0,
-  taxPrice: undefined,
-  shippingPrice: undefined,
+  taxPrice: 0,
+  shippingPrice: 0,
   totalPrice: 0,
   paymentMethod: undefined,
   shippingAddress: undefined,
