98087cb340ebe0391683ad6301f0af2d3b8520db
ILYOSBEK <ilyosbeksalayev240@gmail.com>
2025-02-27 15:31:46 +0500

    update

--- components/shared/header/search.tsx
+++ components/shared/header/search.tsx
@@ -21,9 +21,9 @@ export default async function Search() {
 
   const t = await getTranslations()
   return (
-    <form action='/search' method='GET' className='flex  items-stretch h-10 '>
+    <form action='/search' method='GET' className='flex flex-wrap md:flex-nowrap items-stretch h-10 w-full'>
       <Select name='category'>
-        <SelectTrigger className='w-auto h-full dark:border-gray-200 bg-gray-100 text-black border-r  rounded-r-none rounded-l-md rtl:rounded-r-md rtl:rounded-l-none  '>
+        <SelectTrigger className='w-full md:w-auto h-full dark:border-gray-200 bg-gray-100 text-black border-r rounded-md md:rounded-r-none md:rounded-l-md rtl:rounded-r-md rtl:rounded-l-none'>
           <SelectValue placeholder={t('Header.All')} />
         </SelectTrigger>
         <SelectContent position='popper'>
@@ -36,14 +36,14 @@ export default async function Search() {
         </SelectContent>
       </Select>
       <Input
-        className='flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base h-full'
+        className='flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base h-full w-full md:w-auto'
         placeholder={t('Header.Search Site', { name })}
         name='q'
         type='search'
       />
       <button
         type='submit'
-        className='bg-primary text-primary-foreground text-black rounded-s-none rounded-e-md h-full px-3 py-2 '
+        className='bg-primary text-primary-foreground text-black rounded-md md:rounded-s-none md:rounded-e-md h-full w-full md:w-auto px-3 py-2 flex items-center justify-center'
       >
         <SearchIcon className='w-6 h-6' />
       </button>
